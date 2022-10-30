0188cecbe9a16bd5675986f96440684937e089a9
Hujaifa21 <21hujaifa@gmail.com>
2022-10-30 20:21:14 +0600

    structured by tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Form from "./components/Form";
+import Graph from "./components/Graph";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="text-center">
+      <div className="container bg-gray-800 py-6 my-6 mx-auto">
+        <h1 className="text-center text-3xl text-white">Expense Tracker</h1>
+      </div>
+      <div className="grid md:grid-cols-2 gap-4">
+        <Graph/>
+        <Form/>
+      </div>
     </div>
   );
 };
