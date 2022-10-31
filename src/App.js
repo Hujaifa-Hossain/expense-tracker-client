f144091c7d67eff31876e308563eb2ce7dc18f0b
Hujaifa21 <21hujaifa@gmail.com>
2022-10-31 13:43:17 +0600

    graph added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Graph from "./components/Graph";
 const App = () => {
   return (
     <div className="text-center">
-      <div className="container bg-gray-800 py-6 my-6 mx-auto">
+      <div className="bg-gray-800 py-6 m-5">
         <h1 className="text-center text-3xl text-white">Expense Tracker</h1>
       </div>
-      <div className="grid md:grid-cols-2 gap-4">
-        <Graph/>
-        <Form/>
+      <div className="grid md:grid-cols-2 gap-4 my-6">
+        <Graph />
+        <Form />
       </div>
     </div>
   );
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart, ArcElement } from 'chart.js'
+
+Chart.register(ArcElement)
+
+const config = {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4,
+      borderRadius: 30,
+      spacing: 10,
+    }]
+  },
+  options: {
+    cutout: 115
+  }
+};
+
 
 const Graph = () => {
   return (
-    <div>
-      Graaaph
+    <div className="flex mx-auto max-w-xs">
+      <div className='item'>
+        <div className="chart relative">
+          <Doughnut {...config} />
+          <h3 className='font-bold top-20 left-20 absolute'>
+            Total <span className='block text-emerald-400 font-bold'>${0}</span>
+          </h3>
+        </div>
+      </div>
+      <div>hello</div>
     </div>
   );
 };
